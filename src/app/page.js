c088da8e06e736b5f5c0e26f65689e8dc8e1e9ab
wolfiee42 <saifalislam2022@gmail.com>
2024-01-15 08:35:43 +0600

    initializing recipe app

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import ingredients from '../../ingredients.json'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-center">Recipe app</h1>
+    </main>
+  )
+}
